40fbd16ada76a41b3611d3ae7c88685a0887a848
VictorRivero1204 <victor.rivero1204@gmail.com>
2026-07-31 17:47:44 -0400

    Oasis Of Life V1.0

---
+++ js/script.js
@@ -0,0 +1,71 @@
+const about_slides = document.querySelectorAll(".about-slide");
+const text_slides = document.querySelectorAll(".text-slide");
+
+let index = 0;
+
+
+function cambiarImagen(){
+
+    about_slides[index].classList.remove("active");
+    text_slides[index].classList.remove("active");
+
+
+    index++;
+
+
+    if(index >= about_slides.length){
+
+        index = 0;
+
+    }
+
+
+    about_slides[index].classList.add("active");
+    text_slides[index].classList.add("active");
+
+}
+
+setInterval(cambiarImagen,10000);
+
+const slides = document.querySelectorAll(".slide");
+
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+
+}
+
+next.addEventListener("click", function(){
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+
+});
+
+prev.addEventListener("click", function(){
+
+    currentSlide--;
+
+    if(currentSlide < 0){
+        currentSlide = slides.length - 1;
+    }
+
+    showSlide(currentSlide);
+
+});
+
+showSlide(currentSlide);
